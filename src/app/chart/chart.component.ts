265c8c3cfa32b2dd30369d4ddc44f248fda9acc7
ismail <ialaoui143@gmail.com>
2021-12-19 11:37:58 +0100

    first commit

---
+++ src/app/chart/chart.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import  {Chart, registerables} from 'chart.js'
+
+
+
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.css']
+})
+export class ChartComponent implements OnInit {
+
+  constructor() { 
+    Chart.register(...registerables);
+  }
+
+
+  canvas : any;
+  ctx : any;
+
+
+
+  @ViewChild('mychart') mychart : any;
+
+
+  ngAfterViewInit(){
+    this.canvas = this.mychart.nativeElement;
+    this.ctx = this.canvas.getContext('2d');
+
+
+    new Chart(this.ctx, {
+      type : 'line',
+      data : {
+        datasets : [{
+          label : 'Current Value',
+          data : [20,50,30,20,30],
+          backgroundColor : "#F9C5D5",
+          borderColor : "DA3A80",
+          fill : true
+        }, {
+          label : 'Invested Amount',
+          data : [50,70,40,60,80],
+          backgroundColor : "#47a0e8",
+          borderColor : "#007ee7",
+          fill : true
+        }],
+        labels : ['January 2019', 'February 2019','March 2019','April 2019','','','',]
+      }
+    }
+      
+    );
+  }
+  ngOnInit(): void {
+  }
+
+}
